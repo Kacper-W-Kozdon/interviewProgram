f4e095bb81ff9feb065c5e94fa9e5e68a7d68f42
Kacper Kożdoń <102428159+Kacper-W-Kozdon@users.noreply.github.com>
2023-10-28 14:20:05 +0200

    Add files via upload
    wrappers inside classes

--- Llist.py
+++ Llist.py
@@ -24,15 +24,23 @@ class Llist():
        
         return self
     
-    def append_list(fun, value):
-        if type(value) == list:
-            value.reverse()
-            while(value[-1]):
-                value_to_append = value.pop()
-                fun(value_to_append)
-        else:
-            fun(value)
-        
+    def append_list(fun):
+        def inner_append_list(self, value):
+            if type(value) == list:
+                print("yes")
+                value.reverse()
+                while(value[-1]):
+                    value_to_append = value.pop()
+                    fun(self, value_to_append)
+                    try:
+                        value[-1]
+                    except:
+                        break
+            else:
+                fun(self, value)
+        return inner_append_list
+            
+            
     
     @append_list
     def append(self, value):
@@ -61,6 +69,20 @@ class Llist():
         else: 
             raise StopIteration
         return self
+    
+def main():
+    head = Node(1)
+    llist = Llist(head)
+    print(type(llist))
+    llist.append([2, 3, 4])
+    iterllist = iter(llist)
+    print(next(iterllist))
+    print(next(iterllist))
+    print(next(iterllist))
+    pass
+
+if __name__ == "__main__":
+    main()
     
 
         
