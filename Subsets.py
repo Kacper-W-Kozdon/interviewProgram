30d8ff336bd9e84173e805e982a74c09053f6f6d
Kacper Kożdoń <102428159+Kacper-W-Kozdon@users.noreply.github.com>
2023-11-01 19:50:10 +0100

    Add files via upload

--- Subsets.py
+++ Subsets.py
@@ -4,7 +4,7 @@ Created on Wed Nov  1 17:09:45 2023
 
 @author: xBubblex
 """
-
+depth = 0
 subset = []
 lst_of_subsets = []
 
@@ -20,18 +20,30 @@ def assert_is_set(fun):
         return fun(lst)
     return wrapper
 
+def helper_fun(lst):
+    global subset
+    global list_of_subsets
+    subset += ["end"] 
+    for idx in lst:
+        subset[-1] = lst[idx]
+        list_of_subsets += [subset]
+        
+
 @assert_types
 @assert_is_set
 def main(lst: list,/ ) -> list:
     global depth
     global subset
     global lst_of_subsets
+    helper_lst = lst
     
     for sequence_len in range(len(lst) + 1):
-        for depth in range(sequence_len - 1, -1, -1):
-            pass
+        for depth in range(sequence_len):
+            helper_lst = lst[depth : ]
+            for idx in range(depth, len(lst) - sequence_len + depth):
+                helper_fun(helper_lst)
     pass
 
 if __name__ == "__main__":
     lst = [1, 2, 3, 4]
-    main()4
+    main()
