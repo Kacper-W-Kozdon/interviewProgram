5705db6c3da7bcd168b170da6cd47ea57c59c1a4
Kacper Kożdoń <102428159+Kacper-W-Kozdon@users.noreply.github.com>
2024-04-13 14:13:46 +0200

    Add files via upload

--- GUI/learning-gui.py
+++ GUI/learning-gui.py
@@ -9,31 +9,18 @@ class Window():
         
         self._shape_exists = False
         self._progress_window_exists = False
+        self.progress_root = None
+        self.canvas = None
+        self.line = None
+        self.step = 10
         # Creating the tkinter Window
+        self.main_window()
         
-        
-
-        self.root = Tk()
-        self.root.geometry("512x256")
-
-
-
-        # Button for closing 
-        exit_button = Button(self.root, text="Exit", command=self.Close)
-        exit_button.pack(pady=20)
-
-        progress_button = Button(self.root, text="Progress window", command=Window.progress_window)
-        progress_button.pack(pady=20)
-
-        shape_button = Button(self.root, text="Make shape", command=self.set_shape)
-        shape_button.pack(pady=20)
-
-        self.root.mainloop()
 
     def Close(self):
         self.root.destroy()
 
-    def progress_window():
+    def progress_window(self):
 
         def increment():
             progress_bar.step(20)
@@ -43,10 +30,16 @@ class Window():
 
         def display():
             print(progress_bar["value"])
-
-        root = Tk()
-        root.geometry("400x100")
-        frame = Frame(root)
+        try:
+            if self.progress_root is None:
+                print("Opening new window")
+                self.progress_root = Tk()
+            self.progress_root.geometry("400x100")
+        except TclError:
+            print("Opening new window")
+            self.progress_root = Tk()
+            
+        frame = Frame(self.progress_root)
         progress_bar = Progressbar(frame, mode='determinate')
         progress_bar.pack(padx=10, pady=10)
 
@@ -63,21 +56,47 @@ class Window():
         sizegrip.pack(expand=True, fill=tk.BOTH, anchor=tk.SE)
 
         frame.pack(padx=5, pady=5)
-        root.mainloop()
 
+        self._progress_window_exists = True
+        
+        self.progress_root.mainloop()
+        
     def set_shape(self):
-        if self._shape_exists == True:
+        if self._shape_exists is True:
             return None
+        move_it_button = Button(self.root, text="Move it", command = self.make_it_move)
+        move_it_button.pack()
 
-        canvas = Canvas(self.root, bg="blue", height=100, width=100)
-
-        line = canvas.create_line(10,10,200,200, fill='white')
-        canvas.pack()
+        self.canvas = Canvas(self.root, bg="blue", height=200, width=200)
+        
+        self.line = self.canvas.create_line(0,0,0,200, fill='white')
+        self.canvas.pack()
+        
         self._shape_exists = True
-
-        del_shape_button = Button(self.root, text="Remove shape", command= lambda: (canvas.pack_forget(), del_shape_button.pack_forget(), self.__setattr__("_shape_exists", False)))
+        del_shape_button = Button(self.root, text="Remove shape", command= lambda: (self.canvas.pack_forget(), del_shape_button.pack_forget(), move_it_button.pack_forget(), self.__setattr__("_shape_exists", False)))
         del_shape_button.pack()
 
+    def make_it_move(self):
+        self.step = 10 if (self.canvas.coords(self.line)[0] < 200 and self.step == 10) else -10
+        self.step = -10 if (self.canvas.coords(self.line)[0] > 0 and self.step == -10) else 10
+
+        self.canvas.move(self.line, self.step, 0)
+
+    def main_window(self):
+        self.root = Tk()
+        self.root.geometry("512x512")
+        
+        # Button for closing 
+        exit_button = Button(self.root, text="Exit", command=self.Close)
+        exit_button.pack(pady=20)
+
+        progress_button = Button(self.root, text="Progress window", command=self.progress_window)
+        progress_button.pack(pady=20)
+
+        shape_button = Button(self.root, text="Make shape", command=self.set_shape)
+        shape_button.pack(pady=20)
+
+        self.root.mainloop()
 
 
 # Running test window
