19ecdc356bb1d0ad29000536ef9939c861a03373
Kacper Kożdoń <102428159+Kacper-W-Kozdon@users.noreply.github.com>
2024-04-14 13:06:55 +0200

    Add files via upload

--- GUI/learning-gui.py
+++ GUI/learning-gui.py
@@ -1,6 +1,7 @@
+import time
 import tkinter as tk
-from tkinter import *
 from tkinter import messagebox
+from tkinter import *
 from tkinter.ttk import *
 
 
@@ -13,6 +14,7 @@ class Window():
         self.canvas = None
         self.line = None
         self.step = 10
+        self.animate = False
         # Creating the tkinter Window
         self.main_window()
         
@@ -64,23 +66,41 @@ class Window():
     def set_shape(self):
         if self._shape_exists is True:
             return None
-        move_it_button = Button(self.root, text="Move it", command = self.make_it_move)
+        move_it_button = Button(self.root, text="Move it", command=self.make_it_move)
         move_it_button.pack()
 
-        self.canvas = Canvas(self.root, bg="blue", height=200, width=200)
+        stop_it_button = Button(self.root, text="Stop it", command=self.make_it_stop)
+        stop_it_button.pack()
+
+        self.canvas = Canvas(self.root, bg="black", height=200, width=200)
         
         self.line = self.canvas.create_line(0,0,0,200, fill='white')
         self.canvas.pack()
         
         self._shape_exists = True
-        del_shape_button = Button(self.root, text="Remove shape", command= lambda: (self.canvas.pack_forget(), del_shape_button.pack_forget(), move_it_button.pack_forget(), self.__setattr__("_shape_exists", False)))
+
+        del_shape_button = Button(self.root, text="Remove shape", 
+                                  command= lambda: (self.canvas.pack_forget(), del_shape_button.pack_forget(),
+                                  move_it_button.pack_forget(), stop_it_button.pack_forget(), self.__setattr__("_shape_exists", False)))
         del_shape_button.pack()
 
-    def make_it_move(self):
-        self.step = 10 if (self.canvas.coords(self.line)[0] < 200 and self.step == 10) else -10
-        self.step = -10 if (self.canvas.coords(self.line)[0] > 0 and self.step == -10) else 10
+    def update_line(self):
+        if self.animate is True:
+            self.step = 10 if (self.canvas.coords(self.line)[0] < 200 and self.step == 10) else -10
+            self.step = -10 if (self.canvas.coords(self.line)[0] > 0 and self.step == -10) else 10
 
-        self.canvas.move(self.line, self.step, 0)
+            self.canvas.move(self.line, self.step, 0)
+        
+            self.root.after(50, self.update_line)
+
+
+    def make_it_move(self):
+        self.animate = True
+        self.root.after(50, self.update_line)
+
+    def make_it_stop(self):
+        self.animate = False
+
 
     def main_window(self):
         self.root = Tk()
@@ -95,6 +115,8 @@ class Window():
 
         shape_button = Button(self.root, text="Make shape", command=self.set_shape)
         shape_button.pack(pady=20)
+
+        
 
         self.root.mainloop()
 
